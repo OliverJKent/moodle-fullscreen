6b3391730850fa003c86eea2b16d5ebc44229f0f
Neill Magill <neill.magill@nottingham.ac.uk>
2018-05-21 10:37:12 +0100

    MDLE-1068 Version bump

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
 }
 
-$plugin->version = 2017110200;
+$plugin->version = 2018051800;
 $plugin->component = 'local_fullscreen';
-$plugin->release = '2.7.0 (2017-11-02)';
-$plugin->requires = 2017050500;
+$plugin->release = '2.8.0 (2018-05-18)';
+$plugin->requires = 2017110800;
 $plugin->maturity = MATURITY_STABLE;
